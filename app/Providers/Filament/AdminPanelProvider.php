c7544ad48739d09eb21dc33306f5ed7f26bfd25f
klavsveg99 <klavsveg99@users.noreply.github.com>
2026-08-13 20:27:19 +0300

    Custom login page: title Pierakstīties, teal accent button

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Admin\Auth\Login;
 use App\Filament\Admin\Widgets\CalendarViewings;
 use App\Filament\Admin\Widgets\CrmStatsOverview;
 use App\Filament\Admin\Widgets\DealsByStage;
@@ -75,6 +76,6 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->login();
+            ->login(Login::class);
     }
 }
