869b6eaf05a4ef23bdf5a5f1b62e9b0553508af5
klavsveg99 <klavsveg99@users.noreply.github.com>
2026-08-13 03:17:02 +0300

    Add TrustCrmHost to global middleware for redirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'trust.host' => TrustCrmHost::class,
         ]);
+        $middleware->prepend([
+            TrustCrmHost::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
