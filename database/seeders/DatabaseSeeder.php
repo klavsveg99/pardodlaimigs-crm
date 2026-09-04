b3255cc7c095382b7e1ab3709577a6d172279d22
klavsveg99 <klavsveg99@users.noreply.github.com>
2026-09-04 14:26:42 +0300

    Set admin password to Admin123!, add agent Rob (roberts@) and assign properties to him

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,16 +12,32 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        User::create([
-            'name' => 'Roberts Evarsons',
-            'email' => 'info@pardodlaimigs.lv',
-            'email_verified_at' => now(),
-            'role' => 'admin',
-            'phone' => '+371 24 922 942',
-            'description' => 'Nodrošinu profesionālu atbalstu nekustamā īpašuma realizācijā. Sadarbība ar mani ir pilnībā caurskatāma un saprotama. Jums būs skaidrs kā rodas īpašuma vērtība.',
-            'password' => '$2y$12$1PDtjn/kk5bTkGSFFelkf.K9gtI0PdxnKXDSUDGA6QX21EFLlD2Bm',
-            'calendar_token' => 'hhEaJESI6euav6L7DWtoFH7kuqhtjJZFl871dRSYNgn933RHPVK6s60HhUe1',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'info@pardodlaimigs.lv'],
+            [
+                'name' => 'Roberts Evarsons',
+                'email_verified_at' => now(),
+                'role' => 'admin',
+                'phone' => '+371 24 922 942',
+                'description' => 'Nodrošinu profesionālu atbalstu nekustamā īpašuma realizācijā. Sadarbība ar mani ir pilnībā caurskatāma un saprotama. Jums būs skaidrs kā rodas īpašuma vērtība.',
+                'password' => bcrypt('Admin123!'),
+                'calendar_token' => 'hhEaJESI6euav6L7DWtoFH7kuqhtjJZFl871dRSYNgn933RHPVK6s60HhUe1',
+                'linkedin_url' => 'https://www.linkedin.com/in/roberts-evarsons-861151106/',
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'roberts@pardodlaimigs.lv'],
+            [
+                'name' => 'Roberts Evarsons',
+                'email_verified_at' => now(),
+                'role' => 'aģents',
+                'phone' => '+371 24 922 942',
+                'position' => 'Nekustamā īpašuma aģents',
+                'description' => 'Vairāk kā desmit gadu pieredze pārdošanas jomā mani ir novedusi pie nekustamo īpašumu tirdzniecības Latvijā. Pārdošana ir joma, kurā strādājot es jūtos savā vietā. Es zinu, cik reizēm pārdošanas process var likties sarežģīts, tomēr gadiem ejot esmu radis uz sarežģītām situācijām skatīties viegli un prasmīgi tās atrisināt.',
+                'linkedin_url' => 'https://www.linkedin.com/in/roberts-evarsons-861151106/',
+            ]
+        );
 
         $this->call(CrmPropertySeeder::class);
     }
