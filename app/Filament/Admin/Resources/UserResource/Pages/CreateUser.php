9e81acc95cfb0b5db4725f8111445ac8bb2b11e5
klavsveg99 <klavsveg99@users.noreply.github.com>
2026-09-09 22:33:46 +0300

    Aģenti: Create page title 'Jauns aģents' (nevis 'Jauns lietotājs')

--- app/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
@@ -8,4 +10,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    public function getTitle(): string
+    {
+        return 'Jauns aģents';
+    }
 }
