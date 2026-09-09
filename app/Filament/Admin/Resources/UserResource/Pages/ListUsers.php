9f9d52e5ae4c6aa7160c86393213dbe366323a66
klavsveg99 <klavsveg99@users.noreply.github.com>
2026-09-09 22:09:36 +0300

    Aģenti: 'Pievienot aģentu' poga saraksta galvenē (tikai admin)

--- app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
@@ -3,9 +3,20 @@
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->label('Pievienot aģentu')
+                ->icon('heroicon-o-user-plus')
+                ->color('primary'),
+        ];
+    }
 }
